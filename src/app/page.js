a4cd3c0977758b5c03406cd18fe616eea8cbeccc
VincentCastig <vincent.castig@gmail.com>
2024-08-15 12:23:02 -0500

    style cards

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ const Home = () => (
   <div className="container pb-80">
     <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30' />
     <div className="row">
-      <div className="col col_4">
-        <Card label='Opinions' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' />
+      <div className="col col_4 m-mw-100">
+        <Card label='Opinions' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
       </div>
-      <div className="col col_4">
-        <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' />
+      <div className="col col_4 m-mw-100">
+        <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
       </div>
-      <div className="col col_4">
-        <Card label='Travel Guides' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' />
+      <div className="col col_4 m-mw-100">
+        <Card label='Travel Guides' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
       </div>
     </div>
   </div>
