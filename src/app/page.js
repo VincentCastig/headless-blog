172780a352bc70e7bf06607812ca50284a44c8a6
VincentCastig <vincent.castig@gmail.com>
2024-08-15 20:38:11 -0500

    fetch blog data from cms

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,47 @@
 import Card from "@/components/card/card";
+import config from '../config';
 
-const Home = () => (
-  <div className="container pb-80">
-    <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30' />
-    <div className="row">
-      <div className="col col_4 m-mw-100">
-        <Card label='Opinions' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
-      </div>
-      <div className="col col_4 m-mw-100">
-        <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
-      </div>
-      <div className="col col_4 m-mw-100">
-        <Card label='Travel Guides' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30'/>
+const fetchBlogs = async (params) => {
+  try {
+    const requestOptions = {
+      headers: {
+        'Authorization': `Bearer ${process.env.API_TOKEN}`
+      }
+    };
+    const response = await fetch(`${config.api}/api/blogs?populate=*&${params}`, requestOptions);
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+    return [];
+  }
+};
+
+const Home = async () => {
+  const featuredBlogs = await fetchBlogs('filters[IsFeatured][$eq]=true');
+  const blogs = await fetchBlogs('filters[IsFeatured][$eq]=false');
+
+  console.log('featuredBlogs ', featuredBlogs)
+  console.log('blogs ', blogs)
+
+  return (
+    <div className="container pb-80">
+      {featuredBlogs.map(featuredBlog => (
+        <Card label={featuredBlog.attributes.Category} title={featuredBlog.attributes.Title} summary={featuredBlog.attributes.Summary} href={featuredBlog.attributes.slug} className='mb-30' key={featuredBlog.id} />
+      ))}
+      <div className="row">
+        <div className="col col_4 m-mw-100">
+          <Card label='Opinions' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30' />
+        </div>
+        <div className="col col_4 m-mw-100">
+          <Card label='Product Reviews' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30' />
+        </div>
+        <div className="col col_4 m-mw-100">
+          <Card label='Travel Guides' title='Title of Your Content' summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tortor pretium convallis.' href='/#' className='mb-30' />
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+}
 
 export default Home;
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -5,36 +5,37 @@ import ConditionalRenderer from '../conditional-renderer';
 // import { iconTypes } from '../ButtonIcon/ButtonIcon';
 
 
- 
-const Card = ({buttonText, className, label, title, summary, href}) => {
+
+const Card = ({ buttonText, className, label, title, summary, href }) => {
   return (
     <div className={`${styles.cardWrap} ${className || ''}`}>
-    <div className={`${styles.card}`}>
-      <div className={styles.cardImageWrap}>
-        <div className={styles.cardImage}>
-          <Image src={'/download.png'}
-        alt='Placeholder Image'
-        fill={true} />
+      <div className={`${styles.card}`}>
+        <div className={styles.cardImageWrap}>
+          <div className={styles.cardImage}>
+            <Image src={'/download.png'}
+              alt='Placeholder Image'
+              fill={true} sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            />
+          </div>
+        </div>
+        <div className={styles.cardContent}>
+          <ConditionalRenderer condition={label}>
+            <div className={`${styles.cardLabel} h6 mb-10 c-orange`}>{label}</div>
+          </ConditionalRenderer>
+          <ConditionalRenderer condition={title}>
+            <h3 className={`${styles.cardTitle} h3 mb-20`}>{title}</h3>
+          </ConditionalRenderer>
+          <ConditionalRenderer condition={summary}>
+            <p className={styles.cardSummary}>{summary}
+            </p>
+          </ConditionalRenderer>
+          <ConditionalRenderer condition={href}>
+            <Button href={href}>
+              Read More
+            </Button>
+          </ConditionalRenderer>
         </div>
       </div>
-      <div className={styles.cardContent}>
-        <ConditionalRenderer condition={label}>
-        <div className={`${styles.cardLabel} h6 mb-10 c-orange`}>{label}</div>
-        </ConditionalRenderer>
-        <ConditionalRenderer condition={title}>
-        <h3 className={`${styles.cardTitle} h3 mb-20`}>{title}</h3>
-        </ConditionalRenderer>
-        <ConditionalRenderer condition={summary}>
-        <p className={styles.cardSummary}>{summary}
-        </p>
-        </ConditionalRenderer>
-        <ConditionalRenderer condition={href}>
-          <Button href={href}>
-            Read More
-          </Button> 
-          </ConditionalRenderer>
-      </div>
-    </div>
     </div>
   );
 };
